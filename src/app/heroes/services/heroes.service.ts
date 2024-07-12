b7506e49f47d314dd09faf681a79617afe9c13f2
Jota Santos <jailsonsantosdpge@outlook.com>
2024-07-12 18:26:02 -0400

    210. HerosService - CRUD Endpoints

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 
 @Injectable({ providedIn: 'root' })
@@ -26,6 +26,24 @@ export class HeroesService {
       .pipe(
         catchError( error => of(undefined) )
       );
+  }
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+  }
+
+  updateHero( hero: Hero ): Observable<Hero> {
+    if ( !hero.id ) throw Error('Hero id is required')
+
+    return this.httpClient.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHero( id: string ): Observable<boolean> {
+    return this.httpClient.delete<boolean>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( err => of(false) ),
+        map( resp => true)
+      )
   }
 
 }

6095d954b506d50a6a6ee3a3edb00a849d781674
Jota Santos <jailsonsantosdpge@outlook.com>
2024-07-16 13:23:20 -0400

    224. Autenticar y mostrar usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,30 +5,33 @@ import { User } from '../interfaces/user.interface';
 import { Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private baseUrl = environments.baseUrl;
+  private user?: User;
 
-private baseUrl = environments.baseUrl;
-private user?: User
+  constructor(private http: HttpClient) {}
 
-constructor(
-  private http: HttpClient
-) { }
+  // para ter acesso externo
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+    return structuredClone(this.user);
+  }
 
-
-// para ter acesso externo
-get currentUser(): User|undefined {
-  if ( !this.user ) return undefined;
-  return structuredClone( this.user );
-}
-
-login( email: string, password: string): Observable<User> {
-  this.http.get<User>(`${ this.baseUrl }/users/1`)
-    .pipe(
-      tap( user => this.user = user ),
-      tap( user => localStorage.setItem('token', user.id.toString() ) )
+  login(email: string, password: string): Observable<User> {
+    // http.post('login',{ email, password });
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      tap((user) =>
+        localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k')
+      )
     );
-}
+  }
 
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+  
 }
